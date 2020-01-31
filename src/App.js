9eb26ad5dd8d828a1ca310d5590f0e0a8bf28dfa
kunpei kosaka <kosaka@a-n-t.jp>
2020-01-31 13:09:05 +0900

    counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-      <div><h1>Hello world!</h1></div>
-  );
+const App = () => {
+    return (
+        <Counter></Counter>
+    );
+}
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { count : 0 }
+    }
+
+    handlePlus = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
+    handleMinus = () => {
+        this.setState({
+            count: this.state.count - 1
+        })
+    }
+
+    render() {
+        console.log(this.state);
+        return (
+            <div className="ui" style={{margin:'200px'}}>
+                <div>count: {this.state.count}</div>
+                <button onClick={this.handlePlus}>+</button>
+                <button onClick={this.handleMinus}>-</button>
+            </div>
+        )
+    }
 }
 
 export default App;
