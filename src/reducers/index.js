73b3c2e88aca6369b4241e75d1197cb39c479fb7
kunpei kosaka <kosaka@a-n-t.jp>
2020-02-10 18:50:00 +0900

    form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 //reducers
 import { combineReducers } from 'redux' //  reducersを結合する関数
+import { reducer as form } from 'redux-form'
 import events from './events'
 
 //  結合するreducersを列挙する
 //  例：export default combineReducers({ foo, bar, buzz, count })
-export default combineReducers({ events })
+export default combineReducers({ events, form })
